7e3e0f467cdafc45cb331a0a3485545e973d2456
Alim Sayyad <sayyadalim0022@gmail.com>
2025-07-09 09:18:06 +0530

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
             Hi, I'm <span className="name">Alim Sayyad</span>, a passionate Full-Stack Developer with a strong interest in Front-End and Back-End Development. I completed my Bachelor's degree in Computer Science (BCS) from Abeda Inamdar Senior College, Pune, and I am currently pursuing a Master’s degree in Computer Science (MCS) from the same college.
           </p>
           <p>
-            I enjoy solving real-world problems through code and continuously learning new technologies to stay ahead in web development. I am open to freelance projects, internships and collaborations.
+            I enjoy solving real-world problems through code and continuously learning new technologies to stay ahead in Full-Stack Developer. I am open to freelance projects, internships and collaborations.
           </p>
           <p>
            Coding is not just work for me — it’s an art where I leave my signature on every project.
